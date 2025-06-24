71eadd5fec26e6c92c06dbbbedc24d139a39d8f6
Eve-code93 <evelynejose1993@gmail.com>
2025-06-24 14:11:57 +0300

    Recreated backend virtual environment and reinstalled dependencies

---
+++ backend/app/__init__.py
@@ -0,0 +1,18 @@
+# backend/app/__init__.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
