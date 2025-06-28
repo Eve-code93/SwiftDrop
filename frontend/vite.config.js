940cf927d90e83404d94f6bfb9dbc7085c2b985f
Mammet-tech <edwinmammet19@gmail.com>
2025-06-28 15:22:17 +0300

    Update vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/auth": "http://localhost:5000",
+      "/parcels": "http://localhost:5000",
+      "/tags": "http://localhost:5000",
+      "/admin": "http://localhost:5000",
+      "/agent": "http://localhost:5000",
+      "/tracking-logs": "http://localhost:5000",
+    },
+  },
+});
