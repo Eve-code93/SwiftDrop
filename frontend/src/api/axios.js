628373850cc4eda378257b8b468cf0bcf6cb36f1
Mammet-tech <edwinmammet19@gmail.com>
2025-06-28 15:18:29 +0300

    Update axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,24 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // change if backend uses another port
+  baseURL: "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+// Add a request interceptor to include the token in headers if available
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
